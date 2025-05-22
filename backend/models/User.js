bd771ecd29d38813ec85d0f7323efd60a8344195
KonstandinosZisopoulos <152492089+KonstandinosZisopoulos@users.noreply.github.com>
2025-05-22 16:28:47 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,25 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
+// Σχήμα χρήστη
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
   favorites: { type: Array, default: [] },
   points: { type: Number, default: 0 }
+});
+
+// Πριν αποθηκευτεί ο χρήστης, κάνε hash τον κωδικό του
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default mongoose.model('User', userSchema);
